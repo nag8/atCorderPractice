7c6aeb07d162630fa2b62b2ef7f2814a302ca92b
Ken Nagamine <nintia8@gmai.com>
2019-08-03 13:02:05 +0900

    2019/08/03作業分

--- atCorderPractice.py
+++ atCorderPractice.py
@@ -2,7 +2,27 @@
 
 def main():
 
-    testSlackBot()
+    # abc135_a()
+    abc135_b()
 
-def testCmd():
-    pass
+
+
+def abc135_b():
+# https://atcoder.jp/contests/abc135/tasks/abc135_b
+    5
+    5 2 3 4 1
+    
+
+
+
+def abc135_a():
+# https://atcoder.jp/contests/abc135/tasks/abc135_a
+    a = 3
+    b = 16
+    if (a + b) % 2 == 0:
+        print((a + b) // 2)
+    else:
+        print("IMPOSSIBLE")
+
+if __name__ == '__main__':
+    main()
